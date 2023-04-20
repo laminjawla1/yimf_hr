aa1b70afc6abc9aafb6deb4df3aa6575c2e49807
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-20 09:47:38 +0000

    allowed host

--- hr/settings.py
+++ hr/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = str(os.environ.get("DEBUG")) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yimfstaffdatabase.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
