766dd8547e92d6ea2118bb0431e432e541f50e63
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-31 14:28:41 +0000

    order queryset by employee name

--- payroll/forms.py
+++ payroll/forms.py
@@ -5,7 +5,7 @@ from staff.models import Employee
 class PayrollForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PayrollForm, self).__init__(*args, **kwargs)
-        self.fields['employee'].queryset = Employee.objects.all().exclude(employment_status__in=["Resigned", "Fired"])
+        self.fields['employee'].queryset = Employee.objects.all().exclude(employment_status__in=["Resigned", "Fired"]).order_by("employee_name")
     class Meta:
         model = Payroll
         fields = ['employee', 'basic_salary', 'income_tax', 'staff_fin', 'deduction', 'deduction_type']
