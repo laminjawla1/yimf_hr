6a9c6ee7f8c2648938b68b28f09f3b2e2ca245ce
Lamin Jawla <laminjawla1001@gmail.com>
2023-07-20 11:55:37 +0000

    Catch exception from restcountries api

--- payroll/views.py
+++ payroll/views.py
@@ -123,7 +123,7 @@ class UpdatePayrollView(LoginRequiredMixin, UpdateView):
         form.instance.transport_allowance = round(form.instance.basic_salary * 0.2333333333, 4)
         form.instance.responsibility_allowance = round(form.instance.basic_salary * 0.4666666667, 4)
         form.instance.housing_allowance =round( form.instance.basic_salary * 0.1333333333, 4)
-        form.instance.gross_pay = round(form.instance.basic_salary + form.instance.medical_allowance + form.instance.transport_allowance + form.instance.responsibility_allowance + form.instance.housing_allowance, 4)
+        form.instance.gross_pay = round(form.instance.basic_salary + form.instance.medical_allowance + form.instance.transport_allowance + form.instance.responsibility_allowance + form.instance.housing_allowance + form.instance.risk_allowance, 4)
         if form.instance.basic_salary > 2000:
             form.instance.income_tax = round(((((form.instance.gross_pay * 12) - 64000) * (25 / 100)) + 5000) / 12, 4)
         form.instance.sshfc = round((10 / 100) * form.instance.basic_salary, 4)
